bd591eaef9a66e258528d8b47b6531c016022a29
Kalyanbikramadhikari <kalyanad100@gmail.com>
2023-09-20 18:18:47 +0545

    switching-from-start-screen-to-main-screen

---
+++ lib/quiz.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  
+  @override
+  State<StatefulWidget> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,42 +1,29 @@
 import 'package:flutter/material.dart';
 
-class Start_Screen extends StatelessWidget {
-  const Start_Screen({super.key});
+class StartScreen extends StatelessWidget {
+  const StartScreen(this.switchScreen, {super.key});
 
-  void onPressed() {
-    print("hello");
-  }
+  final void Function() switchScreen;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      
-      
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          //setting transparency
-          // way one is using opactiy widget
-          // Opacity(opacity: 0.5,child: Image.asset('assets/images/quiz-logo.png', width: 200,)),
           
-
-          //second way is by using color and then in the color picker using transparency option
-          Image.asset('assets/images/quiz-logo.png', width: 200,color: const Color.fromARGB(132, 255, 255, 255),),
-
-          // Image.asset('assets/images/dice-1.png', width: 100,),
-
-
-
-
-
-          // for the flutter logo
-          // Container(
-          //   child: Image.asset('assets/images/dice-5.jpg'),
-          // ),
-          SizedBox(height: 50),
-
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 200,
+            color: const Color.fromARGB(132, 255, 255, 255),
+          ),
+          const SizedBox(height: 50),
           // for the button
-          ElevatedButton(onPressed: onPressed, child: Text('Click Here'))
+          ElevatedButton(
+              onPressed: () {
+                switchScreen();
+              },
+              child: const Text('Click Here'))
         ],
       ),
     );
